428c7f9fe083cbcbebf2ce7aa18901111aa32205
athallahmaajid <athallahmaajid@gmail.com>
2021-05-23 17:55:37 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:wisata_bandung/main_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Wisata Bandung",
+      theme: ThemeData(),
+      home: MainScreen(),
+    );
+  }
+}
+
